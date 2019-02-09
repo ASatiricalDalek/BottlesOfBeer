74bacbab14d91ef0640399d9c8e2bdfb7ef37185
Connor <dalek124@gmail.com>
2019-02-09 12:35:49 -0500

    Main and Bottles functions implemented

--- BottlesOfBeer/BeerSong.cs
+++ BottlesOfBeer/BeerSong.cs
@@ -10,12 +10,27 @@ namespace BottlesOfBeer
     {
         public void Main()
         {
-
+            Nintey_Nine_Bottles_of_Beer();
         }
 
         private void Nintey_Nine_Bottles_of_Beer()
         {
-
+            string bottles;
+            string bottlesMinus1;
+            for (int i = 99; i <= 0; i--)
+            {
+                bottles = i.ToString();
+                bottlesMinus1 = (i - 1).ToString();
+                Console.WriteLine(bottles + " bottles of beer on the wall " + bottles + " bottles of beer.");
+                if (bottlesMinus1 != "0")
+                {
+                    Console.WriteLine("Take one down, pass it around, " + bottlesMinus1 + " bottles of beer on the wall");
+                }
+                else
+                {
+                    Console.WriteLine("Take one down, pass it around, no more bottles of beer on the wall");
+                }
+            }
         }
     }
 }
